7cb4d54c2de5a9c64a2388448730dcb2642163ce
prChoe <massivcode@gmail.com>
2017-11-07 10:52:57 +0900

    Ver 1.1.9
    Dialog 에 Callback Listener 다는 로직 변경

--- library/src/main/java/com/massivcode/simplepermissionnotice/SimplePermissionNoticeDialog.java
+++ library/src/main/java/com/massivcode/simplepermissionnotice/SimplePermissionNoticeDialog.java
@@ -26,170 +26,180 @@ import java.util.ArrayList;
 
 @SuppressWarnings("unchecked")
 public class SimplePermissionNoticeDialog extends DialogFragment {
-    private ArrayList<PermissionInfo> mMandatoryPermissions;
-    private ArrayList<PermissionInfo> mOptionalPermissions;
-    private String[] mMandatoryPermissionCodes;
-    private String[] mOptionalPermissionCodes;
-    private String[] mPermissionCodes;
-    private UiConfig mDialogUiConfig;
-    private int mItemDividerDrawableResourceId;
-    private SimplePermissionNoticeCallback mCallback;
 
-    private LinearLayout mRootContainer;
-    private LinearLayout mMandatoryPermissionContainer;
-    private TextView mMandatoryPermissionTitleTextView;
-    private RecyclerView mMandatoryPermissionRecyclerView;
-    private LinearLayout mOptionalPermissionContainer;
-    private TextView mOptionalPermissionTitleTextView;
-    private RecyclerView mOptionalPermissionRecyclerView;
-    private TextView mOkTextView;
+  private ArrayList<PermissionInfo> mMandatoryPermissions;
+  private ArrayList<PermissionInfo> mOptionalPermissions;
+  private String[] mMandatoryPermissionCodes;
+  private String[] mOptionalPermissionCodes;
+  private String[] mPermissionCodes;
+  private UiConfig mDialogUiConfig;
+  private int mItemDividerDrawableResourceId;
+  private static SimplePermissionNoticeCallback sCallback;
 
-    public static SimplePermissionNoticeDialog newInstance(ArrayList<PermissionInfo> mandatoryPermissions,
-                                                           ArrayList<PermissionInfo> optionalPermissions,
-                                                           UiConfig dialogUiConfig,
-                                                           int itemDividerDrawableResourceId,
-                                                           SimplePermissionNoticeCallback callback) {
-        SimplePermissionNoticeDialog dialog = new SimplePermissionNoticeDialog();
+  private LinearLayout mRootContainer;
+  private LinearLayout mMandatoryPermissionContainer;
+  private TextView mMandatoryPermissionTitleTextView;
+  private RecyclerView mMandatoryPermissionRecyclerView;
+  private LinearLayout mOptionalPermissionContainer;
+  private TextView mOptionalPermissionTitleTextView;
+  private RecyclerView mOptionalPermissionRecyclerView;
+  private TextView mOkTextView;
 
-        Bundle args = new Bundle();
-        args.putSerializable("mandatoryPermissions", mandatoryPermissions);
-        args.putSerializable("optionalPermissions", optionalPermissions);
-        args.putSerializable("uiConfig", dialogUiConfig);
-        args.putInt("dividerResourceId", itemDividerDrawableResourceId);
-        args.putSerializable("callback", callback);
-        dialog.setArguments(args);
+  public static SimplePermissionNoticeDialog newInstance(
+      ArrayList<PermissionInfo> mandatoryPermissions,
+      ArrayList<PermissionInfo> optionalPermissions,
+      UiConfig dialogUiConfig,
+      int itemDividerDrawableResourceId,
+      SimplePermissionNoticeCallback callback) {
+    SimplePermissionNoticeDialog dialog = new SimplePermissionNoticeDialog();
 
-        return dialog;
+    Bundle args = new Bundle();
+    args.putSerializable("mandatoryPermissions", mandatoryPermissions);
+    args.putSerializable("optionalPermissions", optionalPermissions);
+    args.putSerializable("uiConfig", dialogUiConfig);
+    args.putInt("dividerResourceId", itemDividerDrawableResourceId);
+    dialog.setArguments(args);
+
+    sCallback = callback;
+    return dialog;
+  }
+
+  @Override
+  public void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setCancelable(false);
+
+    Bundle args = getArguments();
+
+    if (savedInstanceState != null) {
+      args = savedInstanceState;
     }
 
+    mMandatoryPermissions = (ArrayList<PermissionInfo>) args
+        .getSerializable("mandatoryPermissions");
+    mOptionalPermissions = (ArrayList<PermissionInfo>) args.getSerializable("optionalPermissions");
+    mDialogUiConfig = (UiConfig) args.getSerializable("uiConfig");
+    mItemDividerDrawableResourceId = args.getInt("dividerResourceId", -1);
+
+    makeCallbackArrays();
+  }
+
+  @Override
+  public void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    outState.putSerializable("mandatoryPermissions", mMandatoryPermissions);
+    outState.putSerializable("optionalPermissions", mOptionalPermissions);
+    outState.putSerializable("uiConfig", mDialogUiConfig);
+    outState.putInt("dividerResourceId", mItemDividerDrawableResourceId);
+    outState.putSerializable("callback", sCallback);
+  }
+
+  private void makeCallbackArrays() {
+    mMandatoryPermissionCodes = new String[mMandatoryPermissions.size()];
+    mOptionalPermissionCodes = new String[mOptionalPermissions.size()];
+    mPermissionCodes = new String[mMandatoryPermissions.size() + mOptionalPermissions.size()];
+
+    for (int i = 0; i < mMandatoryPermissions.size(); i++) {
+      String permissionCode = mMandatoryPermissions.get(i).getPermissionCode();
+      mMandatoryPermissionCodes[i] = permissionCode;
+    }
+
+    for (int i = 0; i < mOptionalPermissions.size(); i++) {
+      String permissionCode = mOptionalPermissions.get(i).getPermissionCode();
+      mOptionalPermissionCodes[i] = permissionCode;
+    }
+
+    int mandatoryPermissionLength = mMandatoryPermissionCodes.length;
+    int optionalPermissionLength = mOptionalPermissionCodes.length;
+
+    System.arraycopy(mMandatoryPermissionCodes, 0, mPermissionCodes, 0, mandatoryPermissionLength);
+    System.arraycopy(mOptionalPermissionCodes, 0, mPermissionCodes, mandatoryPermissionLength,
+        optionalPermissionLength);
+  }
+
+  @Nullable
+  @Override
+  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
+    return inflater.inflate(R.layout.dialog_simple_permission_notice, container, false);
+  }
+
+  @Override
+  public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+
+    mRootContainer = (LinearLayout) view.findViewById(R.id.root_container);
+    mMandatoryPermissionContainer = (LinearLayout) view.findViewById(R.id.mandatory_container_ll);
+    mMandatoryPermissionTitleTextView = (TextView) view.findViewById(R.id.mandatory_title_tv);
+    mMandatoryPermissionRecyclerView = (RecyclerView) view.findViewById(R.id.mandatory_rv);
+    mOptionalPermissionContainer = (LinearLayout) view.findViewById(R.id.optional_container_ll);
+    mOptionalPermissionTitleTextView = (TextView) view.findViewById(R.id.optional_title_tv);
+    mOptionalPermissionRecyclerView = (RecyclerView) view.findViewById(R.id.optional_rv);
+    mOkTextView = (TextView) view.findViewById(R.id.ok_tv);
+
+    init();
+  }
+
+  private void init() {
+    mRootContainer.setBackgroundColor(mDialogUiConfig.getBackgroundColor());
+    mMandatoryPermissionTitleTextView.setText(mDialogUiConfig.getMandatoryPermissionTitle());
+    mMandatoryPermissionTitleTextView.setTextColor(mDialogUiConfig.getTitleColor());
+    mOptionalPermissionTitleTextView.setText(mDialogUiConfig.getOptionalPermissionTitle());
+    mOptionalPermissionTitleTextView.setTextColor(mDialogUiConfig.getTitleColor());
+
+    mOkTextView.setOnClickListener(mOnOkClickListener);
+    mOkTextView.setTextColor(mDialogUiConfig.getOkColor());
+    mOkTextView.setText(mDialogUiConfig.getOkString());
+
+    setPermissionList(mMandatoryPermissions, mMandatoryPermissionContainer,
+        mMandatoryPermissionRecyclerView);
+    setPermissionList(mOptionalPermissions, mOptionalPermissionContainer,
+        mOptionalPermissionRecyclerView);
+  }
+
+  private void setPermissionList(ArrayList<PermissionInfo> target, LinearLayout container,
+      RecyclerView recyclerView) {
+    boolean isEmpty = target == null || target.isEmpty();
+
+    if (isEmpty) {
+      container.setVisibility(View.GONE);
+    } else {
+      container.setVisibility(View.VISIBLE);
+
+      PermissionInfoAdapter adapter = new PermissionInfoAdapter(
+          mDialogUiConfig.getItemLayoutResourceId(), target);
+      recyclerView.setAdapter(adapter);
+
+      if (mItemDividerDrawableResourceId != -1) {
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(getActivity(),
+            DividerItemDecoration.VERTICAL);
+        dividerItemDecoration
+            .setDrawable(ContextCompat.getDrawable(getActivity(), mItemDividerDrawableResourceId));
+        recyclerView.addItemDecoration(dividerItemDecoration);
+      }
+
+      recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+    }
+
+  }
+
+  private View.OnClickListener mOnOkClickListener = new View.OnClickListener() {
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setCancelable(false);
-
-        Bundle args = getArguments();
-
-        if (savedInstanceState != null) {
-            args = savedInstanceState;
-        }
-
-        mMandatoryPermissions = (ArrayList<PermissionInfo>) args.getSerializable("mandatoryPermissions");
-        mOptionalPermissions = (ArrayList<PermissionInfo>) args.getSerializable("optionalPermissions");
-        mDialogUiConfig = (UiConfig) args.getSerializable("uiConfig");
-        mItemDividerDrawableResourceId = args.getInt("dividerResourceId", -1);
-        mCallback = (SimplePermissionNoticeCallback) args.getSerializable("callback");
-
-        makeCallbackArrays();
+    public void onClick(View view) {
+      if (sCallback != null) {
+        sCallback
+            .onDismiss(mPermissionCodes, mMandatoryPermissionCodes, mOptionalPermissionCodes);
+        dismiss();
+      }
     }
+  };
 
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putSerializable("mandatoryPermissions", mMandatoryPermissions);
-        outState.putSerializable("optionalPermissions", mOptionalPermissions);
-        outState.putSerializable("uiConfig", mDialogUiConfig);
-        outState.putInt("dividerResourceId", mItemDividerDrawableResourceId);
-        outState.putSerializable("callback", mCallback);
+  @Override
+  public void onDestroyView() {
+    if (getDialog() != null && getRetainInstance()) {
+      getDialog().setDismissMessage(null);
     }
-
-    private void makeCallbackArrays() {
-        mMandatoryPermissionCodes = new String[mMandatoryPermissions.size()];
-        mOptionalPermissionCodes = new String[mOptionalPermissions.size()];
-        mPermissionCodes = new String[mMandatoryPermissions.size() + mOptionalPermissions.size()];
-
-        for (int i = 0; i < mMandatoryPermissions.size(); i++) {
-            String permissionCode = mMandatoryPermissions.get(i).getPermissionCode();
-            mMandatoryPermissionCodes[i] = permissionCode;
-        }
-
-        for (int i = 0; i < mOptionalPermissions.size(); i++) {
-            String permissionCode = mOptionalPermissions.get(i).getPermissionCode();
-            mOptionalPermissionCodes[i] = permissionCode;
-        }
-
-        int mandatoryPermissionLength = mMandatoryPermissionCodes.length;
-        int optionalPermissionLength = mOptionalPermissionCodes.length;
-
-        System.arraycopy(mMandatoryPermissionCodes, 0, mPermissionCodes, 0, mandatoryPermissionLength);
-        System.arraycopy(mOptionalPermissionCodes, 0, mPermissionCodes, mandatoryPermissionLength, optionalPermissionLength);
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
-        return inflater.inflate(R.layout.dialog_simple_permission_notice, container, false);
-    }
-
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        mRootContainer = (LinearLayout) view.findViewById(R.id.root_container);
-        mMandatoryPermissionContainer = (LinearLayout) view.findViewById(R.id.mandatory_container_ll);
-        mMandatoryPermissionTitleTextView = (TextView) view.findViewById(R.id.mandatory_title_tv);
-        mMandatoryPermissionRecyclerView = (RecyclerView) view.findViewById(R.id.mandatory_rv);
-        mOptionalPermissionContainer = (LinearLayout) view.findViewById(R.id.optional_container_ll);
-        mOptionalPermissionTitleTextView = (TextView) view.findViewById(R.id.optional_title_tv);
-        mOptionalPermissionRecyclerView = (RecyclerView) view.findViewById(R.id.optional_rv);
-        mOkTextView = (TextView) view.findViewById(R.id.ok_tv);
-
-        init();
-    }
-
-    private void init() {
-        mRootContainer.setBackgroundColor(mDialogUiConfig.getBackgroundColor());
-        mMandatoryPermissionTitleTextView.setText(mDialogUiConfig.getMandatoryPermissionTitle());
-        mMandatoryPermissionTitleTextView.setTextColor(mDialogUiConfig.getTitleColor());
-        mOptionalPermissionTitleTextView.setText(mDialogUiConfig.getOptionalPermissionTitle());
-        mOptionalPermissionTitleTextView.setTextColor(mDialogUiConfig.getTitleColor());
-
-        mOkTextView.setOnClickListener(mOnOkClickListener);
-        mOkTextView.setTextColor(mDialogUiConfig.getOkColor());
-        mOkTextView.setText(mDialogUiConfig.getOkString());
-
-        setPermissionList(mMandatoryPermissions, mMandatoryPermissionContainer, mMandatoryPermissionRecyclerView);
-        setPermissionList(mOptionalPermissions, mOptionalPermissionContainer, mOptionalPermissionRecyclerView);
-    }
-
-    private void setPermissionList(ArrayList<PermissionInfo> target, LinearLayout container, RecyclerView recyclerView) {
-        boolean isEmpty = target == null || target.isEmpty();
-
-        if (isEmpty) {
-            container.setVisibility(View.GONE);
-        } else {
-            container.setVisibility(View.VISIBLE);
-
-            PermissionInfoAdapter adapter = new PermissionInfoAdapter(mDialogUiConfig.getItemLayoutResourceId(), target);
-            recyclerView.setAdapter(adapter);
-
-
-            if (mItemDividerDrawableResourceId != -1) {
-                DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL);
-                dividerItemDecoration.setDrawable(ContextCompat.getDrawable(getActivity(), mItemDividerDrawableResourceId));
-                recyclerView.addItemDecoration(dividerItemDecoration);
-            }
-
-            recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        }
-
-    }
-
-    private View.OnClickListener mOnOkClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            if (mCallback != null) {
-                mCallback.onDismiss(mPermissionCodes, mMandatoryPermissionCodes, mOptionalPermissionCodes);
-                dismiss();
-            }
-        }
-    };
-
-    @Override
-    public void onDestroyView() {
-        if (getDialog() != null && getRetainInstance()) {
-            getDialog().setDismissMessage(null);
-        }
-        super.onDestroyView();
-    }
+    super.onDestroyView();
+  }
 }
